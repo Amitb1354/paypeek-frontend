a1751e171526a1e3eed0c0ee457d7d2ff8f7fafc
Amib1354 <87922809+Amib1354@users.noreply.github.com>
2026-06-12 12:53:30 +0545

    Update proxy to point to production backend

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:4000' } }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://paypeek-backend-production.up.railway.app',
+        changeOrigin: true,
+      }
+    }
+  }
 })
